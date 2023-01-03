264b4a2cc9e4df5152d1e17469c227bbf323be56
#Fahim <118614639+tanvirrahmanfahim@users.noreply.github.com>
2023-01-03 21:57:15 +0600

    Update Pycalculator.py

--- Pycalculator.py
+++ Pycalculator.py
@@ -1,25 +1,49 @@
 '''Pycalculator'''
 # Coded by Tanvir
 num1 = float(input("Number: "))
-oparetor = input('''oparetor:
-1.+ 
-2.- 
-3.* 
-4./
-5.^
-Choose: ''')
-num2 = float(input("Number: "))
+oparetor = input('''
+1.Addition(+)
+2.Substraction(-)
+3.Multipication(*)
+4.Division(/)
+5.Power(^)
+6.Factorial(!)
+Choose a oparetor(1/2/3/4/5/6): ''')
 if oparetor == "1":
+    num2 = float(input("Number: "))
     print(num1,"+",num2,"=",num1+num2)
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
 elif oparetor == "2":
+    num2 = float(input("Number: "))
     print(num1,"-",num2,"=",num1-num2)
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
+
 elif oparetor == "3":
+    num2 = float(input("Number: "))
     print(num1,"*",num2,"=",num1*num2)
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
 elif oparetor == "4":
+    num2 = float(input("Number: "))
     print(num1,"/",num2,"=",num1/num2)
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
 elif oparetor == "5":
+    num2 = float(input("Number: "))
     print(num1,"^",num2,"=",num1**num2)
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
+elif oparetor == "6":
+    fac=1
+    while (num1>0):
+        fac=fac*num1
+        num1=num1-1
+    print("Factorial is",fac)
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
 else:
     print("ERROR!!!")
-print("Credit goes to Tanvir")
-print("VISIT https://github.com/tanvirrahmanfahim ")
+    print("Credit goes to Tanvir")
+    print("VISIT https://github.com/tanvirrahmanfahim ")
