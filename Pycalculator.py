427ffec3fa36030789742d40acf8267da4d0d6a1
#Fahim <118614639+tanvirrahmanfahim@users.noreply.github.com>
2023-01-13 23:01:56 +0600

    Update Pycalculator.py

--- Pycalculator.py
+++ Pycalculator.py
@@ -1,49 +1,33 @@
-'''Pycalculator'''
-# Coded by Tanvir
-num1 = float(input("Number: "))
-oparetor = input('''
-1.Addition(+)
-2.Substraction(-)
-3.Multipication(*)
-4.Division(/)
-5.Exponentiation(^)
-6.Factorial(!)
-Choose a oparetor(1/2/3/4/5/6): ''')
+                             '''Pycalculator'''
+num = float(input("\033[0;35m Number: "))
+oparetor = input('''\033[1;33m]
+1.+
+2.-
+3.*
+4./
+5.^
+6.!
+Choose(1/2/3/4/5/6): ''')
 if oparetor == "1":
-    num2 = float(input("Number: "))
-    print(num1,"+",num2,"=",num1+num2)
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
+    num2 = float(input("\033[0;35m Number: "))
+    print("\033[1;34m Answer is:",num+num2)
 elif oparetor == "2":
-    num2 = float(input("Number: "))
-    print(num1,"-",num2,"=",num1-num2)
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
-
-elif oparetor == "3":
-    num2 = float(input("Number: "))
-    print(num1,"*",num2,"=",num1*num2)
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
+    num2 = float(input("\033[0;35m Number: "))
+    print("\033[1;34m Answer is:",num-num2)
+elif oparetor =="3":
+    num2 = float(input("\033[0;35m Number: "))
+    print("\033[1;34m Answer is:",num*num2)
 elif oparetor == "4":
-    num2 = float(input("Number: "))
-    print(num1,"/",num2,"=",num1/num2)
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
+    num2 = float(input("\033[0;35m Number: "))
+    print("\033[1;34m Answer is:",num/num2)
 elif oparetor == "5":
-    num2 = float(input("Number: "))
-    print(num1,"^",num2,"=",num1**num2)
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
+    num2 = float(input("\033[0;35m Number: "))
+    print("\033[1;34m Answer is:",num**num2)
 elif oparetor == "6":
     fac=1
-    while (num1>0):
-        fac=fac*num1
-        num1=num1-1
-    print("Factorial is",fac)
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
+    while (num>0):
+        fac=fac*num
+        num=num-1
+    print("\033[1;34m Ans is:",fac)
 else:
-    print("ERROR!!!")
-    print("Credit goes to Tanvir")
-    print("VISIT https://github.com/tanvirrahmanfahim ")
+    print("\033[0;31m ERROR!!!  \n")
